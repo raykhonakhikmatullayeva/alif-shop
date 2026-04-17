fbf284ce99e0e299832634d4f2b72adbad38a6c2
Rayhona <raya0106777@gmail.com>
2026-04-17 17:04:52 +0500

    feat: Product struct

--- cmd/my.app/main.go
+++ cmd/my.app/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "alif-shop/internal/product"
+
+func main() {
+	p := product.Product{
+		ID:     7291,
+		Name:   "Xiaomi Redmi Note 14",
+		Price:  2499000,
+		Months: 12,
+	}
+	product.PrintProduct(p)
+}
--- internal/product/product.go
+++ internal/product/product.go
@@ -1,1 +1,29 @@
 package product
+
+import "fmt"
+
+type ProductID int
+type Price int
+type Months int
+type Product struct {
+	ID     ProductID
+	Name   string
+	Price  Price
+	Months Months
+}
+
+func MonthlyPayment(p Product) int {
+	if p.Months == 0 || p.Months > 12 {
+		return 0
+	}
+	return int(p.Price) / int(p.Months)
+}
+func PrintProduct(p Product) { // Alif Shop product card
+	fmt.Println("====== Alif Shop ==========")
+	fmt.Printf("ID: %d\n", p.ID)
+	fmt.Printf("Товар: %s\n", p.Name)
+	fmt.Printf("Цена: %d сум\n", p.Price)
+	fmt.Printf("Рассрочка: %d месяцев\n", p.Months)
+	fmt.Printf("В месяц: %d сум\n", MonthlyPayment(p))
+	fmt.Println("===========================")
+}
